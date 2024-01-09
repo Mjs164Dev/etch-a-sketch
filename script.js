f8e9620eb44b2f2afc801efbd9013992d510ae15
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-01-09 16:03:10 -0500

    add entire 16x16 grid

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const gridContainer = document.querySelector('#gridContainer');
 
-function createGridElement() {
-    const gridElement = document.createElement('div');
-    gridElement.classList.add('grid-element-style');
-    gridContainer.appendChild(gridElement);
+function createGrid() {
+    const gridRow = document.createElement('div');
+    gridRow.classList.add('grid-row-style');
+    gridContainer.appendChild(gridRow);
+
+    for (let i = 0; i < 16; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element-style');
+        gridRow.appendChild(gridElement);
+    }
+
 }
 
 //loop through createGridElement function to create blocks in each row
-for (let i = 0; i < 8; i++) {
-    createGridElement();
+for (let i = 0; i < 16; i++) {
+    createGrid();
 }
-
