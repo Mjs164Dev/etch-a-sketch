4a85bdadd6ee3410ca612b1fd6b0b917a6040846
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-01-10 08:52:59 -0500

    add mouse over color change

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const gridContainer = document.querySelector('#gridContainer');
+let rows = prompt('how many rows?');
 
-function createGrid() {
+function createGrid(columns) {
     const gridRow = document.createElement('div');
     gridRow.classList.add('grid-row-style');
     gridContainer.appendChild(gridRow);
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < columns; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element-style');
+        gridElement.id = 'grid-element-id';
         gridRow.appendChild(gridElement);
+
+        // Add event listener to each gridElement
+        gridElement.addEventListener("mouseenter", (e) => {
+            gridElement.style.backgroundColor = '#002A4A';
+            // ... other actions for mouseenter event ...
+        });
+
+        // If needed, add mouseleave event to reset the style
+        gridElement.addEventListener("mouseleave", (e) => {
+            //gridElement.style.transitionDelay = '100ms';
+            gridElement.style.backgroundColor = '#00A1E2';
+            // ... other actions for mouseleave event ...
+        });
     }
 
 }
 
+
+
 //loop through createGridElement function to create blocks in each row
-for (let i = 0; i < 16; i++) {
-    createGrid();
+for (let i = 0; i < rows; i++) {
+    createGrid(rows);
 }
